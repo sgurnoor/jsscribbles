7b705bba0a03195e27d4ade30e34d74ebdd7226b
Gurnoor Singh <gurnoor@umich.edu>
2016-05-21 04:03:00 -0400

    New styles, red shadow, limiting range

--- convert-to-roman.js
+++ convert-to-roman.js
@@ -1,5 +1,11 @@
 function convertToRoman() {
+ var numField = document.getElementById("num");
  var num = document.getElementById("num").value;
+ if(num > 4999 || num < 1) {
+ 	alert("Please choose a number between 1 and 4999");
+ 	numField.value = numField.defaultValue;
+ 	return false;
+ }
  var ans = "";
  var numCheck = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
  var romanCheck = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
